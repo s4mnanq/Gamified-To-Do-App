67994f1b867d214bb5a4513b976c8ee2505b9458
Dr Raspec <leng79867@gmail.com>
2026-01-19 16:59:07 +0700

    initial commit

---
+++ lib/modules/main/widgets/_bottom_nav_bar.dart
@@ -0,0 +1,29 @@
+part of '../screens/main_screen.dart';
+
+class _BottomNavBar extends StatelessWidget {
+  _BottomNavBar();
+
+  final MainController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => BottomNavigationBar(
+        currentIndex: controller.currentIndex.value,
+        onTap: controller.changeTab,
+        selectedItemColor: const Color(0xFF00B235),
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.task), label: 'Tasks'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/modules/main/widgets/_text_widget.dart
@@ -0,0 +1,10 @@
+part of '../screens/home_screen.dart';
+
+class _TextWidget extends StatelessWidget {
+  const _TextWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello, GetX!', style: TextStyle(fontSize: 24));
+  }
+}
