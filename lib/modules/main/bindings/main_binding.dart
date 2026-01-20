7f12509abf579027f0e6521c574319dc24f29607
Dr Raspec <leng79867@gmail.com>
2026-01-20 14:00:54 +0700

    update calendar screen

--- lib/modules/main/bindings/main_binding.dart
+++ lib/modules/main/bindings/main_binding.dart
@@ -1,3 +1,5 @@
+import 'package:gamified_todo_app/modules/main/controllers/calendar_controller.dart';
+import 'package:gamified_todo_app/repositories/calendar_repository.dart';
 import 'package:get/get.dart';
 import '../controllers/main_controller.dart';
 
@@ -5,5 +7,9 @@ class MainBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<MainController>(() => MainController());
+
+    Get.lazyPut<CalendarRepository>(() => CalendarRepository());
+
+    Get.lazyPut<CalendarController>(() => CalendarController());
   }
 }
--- lib/modules/main/screens/calendar_screen.dart
+++ lib/modules/main/screens/calendar_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:gamified_todo_app/theme/theme.dart';
+import 'package:gamified_todo_app/theme/theme_extensions.dart';
+import 'package:get/get.dart';
+import 'package:contribution_heatmap/contribution_heatmap.dart';
 import '../controllers/calendar_controller.dart';
 
 class CalendarScreen extends GetView<CalendarController> {
@@ -7,6 +10,84 @@ class CalendarScreen extends GetView<CalendarController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const .symmetric(horizontal: 24, vertical: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Yearly Progress', style: context.textTheme.titleMedium),
+
+                  const SizedBox(width: 8),
+
+                  Icon(Icons.calendar_month_outlined, color: context.primary),
+                ],
+              ).marginOnly(bottom: 16),
+
+              Container(
+                padding: const .symmetric(vertical: 8, horizontal: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: AppTheme.mediumGray.withValues(alpha: 0.5),
+                ),
+
+                child: SizedBox(),
+              ).marginOnly(bottom: 16),
+
+              Obx(() {
+                return ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: controller.quarters.length,
+                  itemBuilder: (_, index) {
+                    return LayoutBuilder(
+                      builder: (context, constraints) {
+                        // 13 weeks in a quarter
+                        // in one quarter has 3 months base on controller logic
+                        // so in 3 months there are 13 weeks
+                        const weeksInQuarter = 13;
+
+                        // Decide how many "units" total (cells + spacing)
+                        final maxWidth = constraints.maxWidth;
+
+                        // Compute base cell size
+                        // Why * 1.2 ? to account for spacing between cells
+                        // 1 cell + 0.2 spacing
+                        double cellSize = maxWidth / (weeksInQuarter * 1.2);
+
+                        // Clamp cell size for usability
+                        // this .clamp(min, max) ensures the cell size stays within a reasonable range
+                        // it helps maintain readability and usability across different screen sizes
+                        // base on constraints size of the parent widget
+                        // this means the final cellSize value is LIMITED between 12.0 and 20.0
+                        cellSize = cellSize.clamp(12.0, 20.0);
+
+                        // Spacing derived from cell size
+                        final cellSpacing = (cellSize * 0.15).clamp(1.5, 3.0);
+
+                        return Center(
+                          child: ContributionHeatmap(
+                            cellSize: cellSize,
+                            cellSpacing: cellSpacing,
+                            padding: EdgeInsets.zero,
+                            entries: controller.quarters[index],
+                          ),
+                        ).marginOnly(right: 16);
+                      },
+                    );
+                  },
+                  separatorBuilder: (_, _) => const SizedBox(height: 24),
+                );
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
